303b66179a87b05965aca240d056d8c37aefb5b9
hls93 <hlshaw3@ncsu.edu>
2020-05-05 16:24:44 -0400

    initial commit

---
+++ src/main/java/com/in28mintutes/springBoot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -0,0 +1,19 @@
+package com.in28mintutes.springBoot.basics.springbootin10steps;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringbootIn10StepsApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+		for(String name: applicationContext.getBeanDefinitionNames()){
+			System.out.println(name);
+		}
+	}
+
+}
